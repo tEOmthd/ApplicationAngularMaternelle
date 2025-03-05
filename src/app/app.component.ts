ff50841077b4b21a1ba54151b1f9f1672c6fa99d
teo mathiaud <teo.mathiaud@etu.univ-amu.fr>
2025-03-05 16:46:06 +0100

    Squelette de la classe Article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,3 +9,34 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 export class AppComponent {
   constructor() {}
 }
+
+export class Article {
+  id: number;
+  titre: string;
+  date: Date;
+  categorie: string;
+  important: boolean;
+  classe: number;
+  texte: string;
+  photos: [image: string, legende: string];
+
+  constructor(
+    id: number,
+    titre: string,
+    date: Date,
+    categorie: string,
+    important: boolean,
+    classe: number,
+    texte: string,
+    photos: [image: string, legende: string]
+  ) {
+    this.id = id;
+    this.titre = titre;
+    this.date = date;
+    this.categorie = categorie;
+    this.important = important;
+    this.classe = classe;
+    this.texte = texte;
+    this.photos = photos;
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+  ],
 })
 export class Tab1Page {
   constructor() {}
